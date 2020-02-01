fb84bbd15ad5f0d82fc2ee2c24a66c0ec1adf5fb
Andrey Kim <kimoofey@gmail.com>
2020-02-01 17:29:00 +0300

    added mobx. changed structure

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,97 +1,36 @@
-import React, {useEffect, useReducer} from 'react';
+import React, {Component} from 'react';
 import '../App.css';
-import Header from "./Header";
+import Header from "../view/Header";
 import Search from "./Search";
-import Movie from "./Movie";
-import axios from 'axios'
+import Movie from "../view/Movie";
+import {inject, observer} from "mobx-react";
 
-const MOVIE_API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=d424e613"; // you should replace this with yours
-
-const initialState = {
-  loading: true,
-  movies: [],
-  errorMessage: null,
-};
-
-const reducer = (state: any, action: { type: string; payload?: any; error?: any; }) => {
-  switch (action.type) {
-    case 'SEARCH_MOVIES_REQUEST':
-      return {
-          ...state,
-        loading: true,
-        errorMessage: null,
-      };
-    case 'SEARCH_MOVIES_SUCCESS':
-      return {
-          ...state,
-          loading: false,
-          movies: action.payload,
-      };
-    case 'SEARCH_MOVIES_FAILURE':
-      return {
-          ...state,
-        loading: false,
-        errorMessage: action.error,
-      };
-    default:
-      return state;
-  }
-};
-
-const App: React.FC = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  useEffect(() => {
-      axios.get(MOVIE_API_URL)
-        .then(jsonResponse => {
-          dispatch({
-              type: 'SEARCH_MOVIES_SUCCESS',
-              payload: jsonResponse.data.Search,
-          })
-        })
-  }, []);
-
-  const search = (searchValue: string) => {
-    dispatch({
-      type: 'SEARCH_MOVIES_REQUEST',
-    });
-
-      axios(`https://www.omdbapi.com/?s=${searchValue}&apikey=4a3b711b`)
-        .then(jsonResponse => {
-            if (jsonResponse.data.Response === 'True') {
-                dispatch({
-                    type: 'SEARCH_MOVIES_SUCCESS',
-                    payload: jsonResponse.data.Search,
-                });
-            } else {
-                dispatch({
-                    type: 'SEARCH_MOVIES_FAILURE',
-                    payload: jsonResponse.data.Error,
-                });
-            }
-        })
-  };
-
-  const {movies, errorMessage, loading} = state;
-
-  return (
-    <div className="App">
-      <Header text='HOOKED' />
-      <Search search={search} />
-      <p className='App-intro'>Sharing a few of our favourite movies</p>
-      <div className='movies'>
-        {loading && !errorMessage ? (
-            <span>loading...</span>
-        ) : errorMessage ? (
-            <div className='errorMessage'>{errorMessage}</div>
-        ) : (
-            movies ? movies.map((movie: { Title: any; }, index: any) => (
-                <Movie key={`${index}-${movie.Title}`} movie={movie}/>
-            )) : null
-        )}
-      </div>
-    </div>
-  );
-};
+@inject('rootStore')
+@observer
+class App extends Component {
+    render() {
+        // @ts-ignore
+        const {rootStore} = this.props;
+        return (
+            <div className="App">
+                <Header text='HOOKED'/>
+                <Search/>
+                <p className='App-intro'>Sharing a few of our favourite movies</p>
+                <div className='movies'>
+                    {rootStore.loading && !rootStore.errorMessage ? (
+                        <span>loading...</span>
+                    ) : rootStore.errorMessage ? (
+                        <div className='errorMessage'>{rootStore.errorMessage}</div>
+                    ) : (
+                        // @ts-ignore
+                        rootStore.movies ? rootStore.movies.map((movie, index) => (
+                            <Movie key={`${index}-${movie.Title}`} movie={movie}/>
+                        )) : null
+                    )}
+                </div>
+            </div>
+        )
+    }
+}
 
 export default App;
---
+++ src/constants/api.tsx
@@ -0,0 +1,2 @@
+export const MOVIE_API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=d424e613';
+export const DEFAULT_PLACEHOLDER_IMAGE = 'https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg';
